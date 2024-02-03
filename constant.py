19229849932009164dd169b86cf98319c70eaee5
RuizheX <ruizhexueluzhuan@gmail.com>
2024-02-03 17:28:12 +0800

    add constants

--- constant.py
+++ constant.py
@@ -1,3 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from model import model
 #EAST parameters
 
 #dataloader
@@ -9,6 +13,9 @@ num_workers=4
 
 
 #trainer
+scheduler = None
+epochs = 10
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-
-#model
+save_path='checkpoints/model_debug.pth'
