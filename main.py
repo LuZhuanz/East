3d37df57fec5213290f8a66a4fe407fbbe5e6ccf
睿哲 薛 <201840061@smail.nju.edu.cn>
2024-03-05 23:40:18 +0800

    benchmark_main

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from constant_test import *
 from trainer import train_model
 from dataset import train_loader_, val_loader_, data_loader
-from model import initialize_model
+from model_debug import initialize_model
 
 def main():
     
     model = initialize_model(10).to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
     print(model)
     optimizer = torch.optim.Adam(model.parameters(),lr=lr)
-    train_loader, val_loader = data_loader()
+    #train_loader, val_loader = data_loader()
+    train_loader = train_loader_
+    val_loader = val_loader_
     train_model(model, train_loader, val_loader, criterion, optimizer, scheduler, epochs, device='cuda', save_path = save_path)
     
     
